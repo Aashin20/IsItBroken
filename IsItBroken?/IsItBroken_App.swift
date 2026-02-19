4262e6a4e229e035ace8369f23d9763569dc7a6d
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2026-02-19 17:16:29 +0530

    Initial Commit

---
+++ IsItBroken?/IsItBroken_App.swift
@@ -0,0 +1,17 @@
+//
+//  IsItBroken_App.swift
+//  IsItBroken?
+//
+//  Created by user@33 on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct IsItBroken_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

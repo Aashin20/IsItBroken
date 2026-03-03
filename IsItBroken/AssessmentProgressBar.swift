3d6e3ecfa7c5d03ec2716ac5670ea3da77774327
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2026-03-03 12:27:46 +0530

    Create centralised progress bar

--- IsItBroken/AssessmentProgressBar.swift
+++ IsItBroken/AssessmentProgressBar.swift
@@ -1,1 +1,27 @@
 
+//
+//  AssessmentProgressBar.swift
+//  IsItBroken
+//
+//  Created by user@33 on 26/02/26.
+//
+
+
+import SwiftUI
+
+struct AssessmentProgressBar: View {
+    let currentStep: Int
+
+    private let totalSegments = 5
+
+    var body: some View {
+        HStack(spacing: 6) {
+            ForEach(0..<totalSegments, id: \.self) { i in
+                ProgressSegment(index: i, current: currentStep)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        .padding(.horizontal, 22)
+        .padding(.bottom, 14)
+    }
+}

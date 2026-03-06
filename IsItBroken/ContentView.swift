93e4bb797b1b140bb5d34a44e89a8359e15d64ec
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2026-03-06 11:45:33 +0530

    Remove files

--- IsItBroken/ContentView.swift
+++ IsItBroken/ContentView.swift
@@ -1,24 +1,15 @@
 //
 //  ContentView.swift
-//  IsItBroken
+//  IsItBroken?
 //
-//  Created by user@33 on 25/02/26.
+//  Created by user@33 on 19/02/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        AssessmentOnboardingScreen()
     }
-}
-
-#Preview {
-    ContentView()
+    
 }

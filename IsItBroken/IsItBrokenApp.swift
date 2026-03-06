9c0fc320d7e2663e8509051b8965517d292a6c56
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2026-03-06 11:45:47 +0530

    Create app entry point

--- IsItBroken/IsItBrokenApp.swift
+++ IsItBroken/IsItBrokenApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct IsItBrokenApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AssessmentOnboardingScreen()
         }
     }
 }
